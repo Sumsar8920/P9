6deca26eb12f716be8f6a962f79e1934fb459289
Rasmus <rrosen13@student.aau.dk>
2017-10-04 11:50:20 +0200

    Screen brightness and vibrator
    I have implemented two classes LightSensor and Vibrator which can
    control the screen brightness and make the phone vibrate.

---
+++ P9/app/src/main/java/com/example/rasmus/p9/LightSensor.java
@@ -0,0 +1,27 @@
+package com.example.rasmus.p9;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.WindowManager;
+
+/**
+ * Created by Rasmus on 04-10-2017.
+ */
+
+public class LightSensor {
+    Activity activity;
+
+    //constructor that takes Activity as a parameter
+    public LightSensor(Activity activity){
+        this.activity = activity;
+    }
+
+    //method that changes the screen brightness. The maximum value is 1F and minimum is 0.1F
+    public void lightIntensity(){
+        WindowManager.LayoutParams layout = activity.getWindow().getAttributes();
+        layout.screenBrightness = 0.1F;
+        activity.getWindow().setAttributes(layout);
+    }
+
+
+}
---
+++ P9/app/src/main/java/com/example/rasmus/p9/Vibration.java
@@ -0,0 +1,27 @@
+package com.example.rasmus.p9;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Vibrator;
+
+/**
+ * Created by Rasmus on 04-10-2017.
+ */
+
+public class Vibration {
+
+    Context context;
+
+    //constructor that takes the context as a parameter
+    public Vibration(Context context){
+        this.context = context;
+    }
+
+    //method that vibrates phone. The duration 500 is in milliseconds
+    public void vibrate(){
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
+    }
+
+}
