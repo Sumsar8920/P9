e53e37f96b053d12b18f286cd6a514986c7b1674
Jannik Boldsen <jannikboldsen@gmail.com>
2017-10-11 12:27:41 +0200

    Egg Game V0.1
    First version of the egg game

--- P9/app/src/main/java/com/example/rasmus/p9/MainActivity.java
+++ P9/app/src/main/java/com/example/rasmus/p9/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.rasmus.p9;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public Vibration vibrator;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,17 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //create lightsensor object and pass this activity to it
-        LightSensor lightSensor = new LightSensor(this);
+        // LightSensor lightSensor = new LightSensor(this);
         //call method from LightSensor Class that changes the screen brightness
-        lightSensor.lightIntensity();
+        // lightSensor.lightIntensity();
+
+        button = (Button)findViewById(R.id.button2);
 
         //create vibrator object and pass the context
         Vibration vibrator = new Vibration(this);
         //call method that vibrates the phone
-        vibrator.vibrate();
+        // vibrator.vibrate();
 
-        Flashlight flashlight = new Flashlight(this);
-        flashlight.startLight();
+        //Flashlight flashlight = new Flashlight(this);
+        // flashlight.startLight();
+
+
 
     }
+
+    public void Start (View view){
+
+        Intent intent = new Intent(this, Accelerometer.class);
+        startActivity(intent);
+
+    };
+
+
 }
