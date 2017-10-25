d6203249c46a6bb309789c3c90f4c969849bdeab
Rasmus <rrosen13@student.aau.dk>
2017-10-25 13:09:59 +0200

    Bla
    Branch this Jannik!!

--- P9/app/src/main/java/com/example/rasmus/p9/Location.java
+++ P9/app/src/main/java/com/example/rasmus/p9/Location.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 public class Location {
 
     public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS = 12 * 60 * 60 * 1000;
-    public static final float GEOFENCE_RADIUS_IN_METERS = 100;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 200;
 
     public static final HashMap<String, LatLng> LANDMARKS = new HashMap<String, LatLng>();
     static {
@@ -19,7 +19,7 @@ public class Location {
         LANDMARKS.put("CREATE", new LatLng(57.048424, 9.930262));
 
         // Googleplex.
-        LANDMARKS.put("Home", new LatLng(57.044801, 9.926398));
+        LANDMARKS.put("Cassiopeia", new LatLng(57.012301, 9.991394));
 
         // Test
         //LANDMARKS.put("SFO", new LatLng(37.621313,-122.378955));
