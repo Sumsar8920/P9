91d2e30a1f20931a2bfca365bf33b69754795bc5
Jannik Boldsen <jannikboldsen@gmail.com>
2018-02-26 11:12:16 +0100

    Changes

--- P9/app/src/main/java/com/example/rasmus/p9/Other/GameIntro.java
+++ P9/app/src/main/java/com/example/rasmus/p9/Other/GameIntro.java
@@ -1,6 +1,11 @@
 package com.example.rasmus.p9.Other;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
@@ -11,24 +16,74 @@ import com.example.rasmus.p9.Minigames.Minigame;
 import com.example.rasmus.p9.NavigationMethod.Navigation;
 import com.example.rasmus.p9.R;
 
+import java.io.IOException;
+
 public class GameIntro extends AppCompatActivity {
 
     public TextView txtView;
     int counter = 10;
     public boolean run = false;
+    String playerRole;
+    MediaPlayer mediaPlayer;
+    String game;
+    String file = "one";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_intro);
 
-        txtView = (TextView) findViewById(R.id.countdown);
+        Intent intent = new Intent();
+        game = intent.getStringExtra("GAME");
 
-        startCountdown();
+        SharedPreferences shared = getSharedPreferences("your_file_name", MODE_PRIVATE);
+        playerRole = (shared.getString("PLAYERROLE", ""));
+        mediaPlayer = new MediaPlayer();
+        callCommunicators();
+
+
+
+        /*txtView = (TextView) findViewById(R.id.countdown);
+
+        startCountdown();*/
 
     }
 
-    public void startCountdown(){
+    public void callCommunicators(){
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_VOICE_CALL);
+        if(playerRole .equals("1") || playerRole .equals("3")){
+            if(game.equals("1")){
+                file = "intro";
+                whichSoundFile();
+            }
+            if(game.equals("2")){
+                mediaPlayer.reset();
+                whichSoundFile();
+            }
+            if(game.equals("3")){
+                mediaPlayer.reset();
+                whichSoundFile();
+            }
+        }
+        mediaPlayer.start();
+    };
+
+    public void whichSoundFile(){
+        try {
+            //set datasource to MediaPlayer
+            mediaPlayer.setDataSource(this, Uri.parse("android.resource://" + this.getPackageName() + "/raw/" + file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public void startCountdown(){
         final Handler handler = new Handler();
         final int delay = 1000; //milliseconds
 
@@ -69,5 +124,5 @@ public class GameIntro extends AppCompatActivity {
 
 
 
-    }
+    }*/
 }
